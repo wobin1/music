c05e62052222d2815c88b3cf590ae0746d1fa179
wobin1 <nathanielmusa3@gmail.com>
2019-08-31 15:51:17 +0100

    added the index template

--- hello/blog/views.py
+++ hello/blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context={}
+    return render(request, 'blog/index.html', context)
--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # personally installed apps
+    'blog',
 ]
 
 MIDDLEWARE = [
